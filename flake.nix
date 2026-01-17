3b8fd45e86691e66826dbc0fb80bde3b9c82b765
himkt <himkt@klis.tsukuba.ac.jp>
2026-01-17 23:23:15 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment for not-for-me";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Node.js $(node --version)"
+          '';
+        };
+      }
+    );
+}
